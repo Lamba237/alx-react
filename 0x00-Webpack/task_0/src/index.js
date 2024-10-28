b893c09e4f24ab148573159c13103fc9b1e99ad9
Lamba237 <simontanyi2004@gmail.com>
2024-10-28 23:03:47 +0100

    updates

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,6 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-paragraph_one.innerHTML = "Holberton Dashboard";
-paragraph_two.innerHTML = "Dashboard data for the students";
-paragraph_three.innerHTML = "Copyright - Holberton School";
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
