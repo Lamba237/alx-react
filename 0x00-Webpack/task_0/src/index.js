6820d233c37d00682ee746715684f4e53ffa6a3e
Lamba237 <simontanyi2004@gmail.com>
2024-10-28 22:10:45 +0100

    source directory added

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+paragraph_one.innerHTML = "Holberton Dashboard";
+paragraph_two.innerHTML = "Dashboard data for the students";
+paragraph_three.innerHTML = "Copyright - Holberton School";
