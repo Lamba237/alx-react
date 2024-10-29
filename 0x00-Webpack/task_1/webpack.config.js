fa034ec814e9c52872e91b9f71cf0e5e024e4909
Lamba237 <simontanyi2004@gmail.com>
2024-10-29 06:55:41 +0100

    webpack custom config file added

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
     output: {
-        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
     },
     mode: 'production',
 };
