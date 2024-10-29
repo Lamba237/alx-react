6772116ca832353850d1343fd99347c6660d9cc8
Lamba237 <simontanyi2004@gmail.com>
2024-10-29 07:14:13 +0100

    click events added

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,22 @@
 import $ from 'jquery';
+import { debounce } from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
+
+const button = document.getElementsByTagName('button')[0];
+const countElement = document.getElementById('count');
+// initiliaze click to 0
+let count = 0;
+
+const handleClick = () => {
+    count++;
+    countElement.textContent = `Clicked ${count} times`;
+};
+
+const debouncedHandleClick = debounce(handleClick, 300);
+
+button.addEventListener('click', debouncedHandleClick);
