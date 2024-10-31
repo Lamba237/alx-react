fe919262e39f88e2acca9364bbfa689d87d11490
Lamba237 <simontanyi2004@gmail.com>
2024-10-31 10:04:00 +0100

    major updates

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './Notifications';
+import './Notifications.css';
 
-export function notifications() {
+export default function Notifications() {
     return (
         <div className='Notifications'>
         <p>Here is the list of notifications</p>
--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
+import './Notifications.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div id="root-notifications">
+      <Notifications />
+    </div>
     <App />
   </React.StrictMode>
 );
