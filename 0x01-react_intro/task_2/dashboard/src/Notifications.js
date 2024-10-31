9c2b7df6e9ae1c5a09b7af3e6b8070091c808b02
Lamba237 <simontanyi2004@gmail.com>
2024-10-31 14:30:37 +0100

    updates

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+
+export default function Notifications() {
+    const handleClick = () => {
+        console.log('Close button has been clicked');
+    }
+    return (
+        <div className='Notifications'>
+         <button style={{
+            position: 'absolute',
+            top: '5px',
+            right: '5px',
+            marginTop: '10px',
+            marginRight: '10px',
+         }}
+         aria-label='close'
+         onClick={handleClick}
+         >
+         <img src='close-icon.png' alt='close' style={{
+            width: '15px',
+            height: '15px',
+         }}/>
+         </button>
+         <p>Here is the list of notifications</p>
+         <ul>
+            <li data-priority="default">New course available</li>
+            <li data-priority="urgent">New resume available</li>
+            <li data-priority="urgent" dangerouslySetInnerHTML={{__html: getLatestNotification() }}></li>
+         </ul>
+        </div>
+    );
+}
