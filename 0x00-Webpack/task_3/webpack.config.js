d987b01011e1e9ac5ddb512ba82d184d381986ac
Lamba237 <simontanyi2004@gmail.com>
2024-11-13 23:25:48 +0100

    updates

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,76 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const { entry } = require('../task_1/webpack.config');
+
+module.exports = {
+    entry: {
+        header: './modules/header/header.js',
+        body: './modules/body/body.js',
+        footer: './modules/footer/footer.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        open: true,
+        port: 8564
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+};
